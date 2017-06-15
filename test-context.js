194b9d684c3b7ca8057b23db062dd41692bcb957
Shafeeque <mohamed.m@happiestminds.com>
2017-06-15 16:16:42 +0530

    spec file path changed

--- test-context.js
+++ test-context.js
@@ -1,4 +1,4 @@
 import app from './app/app.module';
-var testsContext = require.context('./app/test/', true, /\.spec\.js/);
+var testsContext = require.context('./app/', true, /\.spec\.js/);
 testsContext.keys().forEach(testsContext);
 console.log(app);
